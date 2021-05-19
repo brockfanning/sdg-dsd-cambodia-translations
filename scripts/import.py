ba76d439a15592670f73ceef6aa5f17bb6a642d0
Brock Fanning <brockfanning@gmail.com>
2021-05-19 10:10:32 -0400

    Only import source language

--- scripts/import.py
+++ scripts/import.py
@@ -3,11 +3,13 @@ import yaml
 import os
 from sdg.translations import TranslationInputSdmx
 
+source_language = 'en'
+
 translation_input = TranslationInputSdmx(source='dsd.xml')
 translation_input.execute()
 translations = translation_input.get_translations()
-for language in translations:
-    for concept in translations[language]:
-        filename = os.path.join('translations', language, concept) + '.yml'
-        with open(filename, 'w', encoding='utf-8') as stream:
-            yaml.dump(translations[language][concept], stream, allow_unicode=True, width=1000)
+
+for concept in translations[source_language]:
+    filename = os.path.join('translations', source_language, concept) + '.yml'
+    with open(filename, 'w', encoding='utf-8') as stream:
+        yaml.dump(translations[source_language][concept], stream, allow_unicode=True, width=1000)
